9d0c3579c9bc27ec50992fb0e68fe1d9fc41b3bc
MegaDood11 <73924042+MegaDood11@users.noreply.github.com>
2025-05-11 20:22:59 +1000

    Add files via upload

---
+++ Original NPCs/Bwooggler/npc-790.lua
@@ -0,0 +1,83 @@
+--[[
+	This template can be used to make your own custom NPCs!
+	Copy it over into your level or episode folder and rename it to use an ID between 751 and 1000. For example: npc-751.lua
+	Please pay attention to the comments (lines with --) when making changes. They contain useful information!
+	Refer to the end of onTickNPC to see how to stop the NPC talking to you.
+]]
+
+
+--NPCManager is required for setting basic NPC properties
+local npcManager = require("npcManager")
+
+--Create the library table
+local sampleNPC = {}
+--NPC_ID is dynamic based on the name of the library file
+local npcID = NPC_ID
+
+--Defines NPC config for our NPC. You can remove superfluous definitions.
+local sampleNPCSettings = {
+	id = npcID,
+
+	-- ANIMATION
+	--Sprite size
+	gfxwidth = 32,
+	gfxheight = 32,
+	--Hitbox size. Bottom-center-bound to sprite size.
+	width = 32,
+	height = 32,
+	--Sprite offset from hitbox for adjusting hitbox anchor on sprite.
+	gfxoffsetx = 0,
+	gfxoffsety = 0,
+	--Frameloop-related
+	frames = 4,
+	framestyle = 1,
+	framespeed = 8, -- number of ticks (in-game frames) between animation frame changes
+
+	foreground = false, -- Set to true to cause built-in rendering to render the NPC to Priority -15 rather than -45
+
+	-- LOGIC
+	--Movement speed. Only affects speedX by default.
+	speed = 1,
+	luahandlesspeed = false, -- If set to true, the speed config can be manually re-implemented
+	nowaterphysics = false,
+	cliffturn = false, -- Makes the NPC turn on ledges
+	staticdirection = false, -- If set to true, built-in logic no longer changes the NPC's direction, and the direction has to be changed manually
+
+	--Collision-related
+	npcblock = false, -- The NPC has a solid block for collision handling with other NPCs.
+	npcblocktop = false, -- The NPC has a semisolid block for collision handling. Overrides npcblock's side and bottom solidity if both are set.
+	playerblock = false, -- The NPC prevents players from passing through.
+	playerblocktop = false, -- The player can walk on the NPC.
+
+	nohurt=false, -- Disables the NPC dealing contact damage to the player
+	nogravity = true,
+	noblockcollision = true,
+	notcointransformable = false, -- Prevents turning into coins when beating the level
+	nofireball = true,
+	noiceball = true,
+	noyoshi= true, -- If true, Yoshi, Baby Yoshi and Chain Chomp can eat this NPC
+
+	lightradius = 75,
+	lightbrightness = 1,
+	lightcolor = Color.white,
+
+	jumphurt = true, --If true, spiny-like (prevents regular jump bounces)
+	spinjumpsafe = true, --If true, prevents player hurt when spinjumping and causes a bounce
+	harmlessgrab = false, --Held NPC hurts other NPCs if false
+	harmlessthrown = false, --Thrown NPC hurts other NPCs if false
+	nowalldeath = false, -- If true, the NPC will not die when released in a wall
+
+	linkshieldable = false,
+	noshieldfireeffect = false,
+
+	grabside=false,
+	grabtop=false,
+}
+
+npcManager.registerDefines(npcID, {NPC.UNHITTABLE})
+
+--Applies NPC settings
+npcManager.setNpcSettings(sampleNPCSettings)
+
+--Gotta return the library table!
+return sampleNPC
